adedf4696717c884bc4fe0bd081aa209dfc90928
xyizkoo <xyizko@outlook.com>
2024-12-21 11:55:07 +0530

    FixSudo

--- x3.sh
+++ x3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apt update && sudo apt upgrade -y
-apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
-apt install xrdp -y
-systemctl status xrdp
-adduser xrdp ssl-cert
-systemctl restart xrdp
-apt-get install gnome-software -y
+sudo apt update && sudo apt upgrade -y
+sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
+sudo apt install xrdp -y
+sudo systemctl status xrdp
+sudo adduser xrdp ssl-cert
+sudo systemctl restart xrdp
+sudo apt-get install gnome-software -y
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
