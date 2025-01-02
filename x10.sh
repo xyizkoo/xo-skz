408aa064d844966cd25bc9415869d01288572213
xyizkoo <xyizko@outlook.com>
2025-01-02 21:38:59 +0530

    x10Update

--- x10.sh
+++ x10.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo apt install openssh-server
+sudo systemctl start ssh 
+sudo systemctl enable ssh
+sudo systemctl restart ssh
 sudo apt install xrdp -y
 sudo systemctl status xrdp
 sudo adduser xrdp ssl-cert
